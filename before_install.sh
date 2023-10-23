f9f2a4b08eb884a09ce1beddce074b1bcd742cde
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-24 02:54:26 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-#
+# Remove existing content in the /var/www/html directory
+rm -rf /var/www/html/*
+
+# Make sure the scripts are executable
+chmod +x before_install.sh
+chmod +x after_install.sh
+chmod +x start_server.sh
+chmod +x stop_server.sh
