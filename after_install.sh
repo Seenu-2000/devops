4cdf5173aae688e8b09bba481c351131f20b8497
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-25 13:12:11 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -e
 
-# This is a comment line
+# This script is intentionally left empty in this example. 
+# You can add post-installation tasks here if needed, such as configuring environment variables or setting up your application's dependencies.
+
+# Navigate to the application directory
+cd /var/www/myapp
+
+# Install Node.js application dependencies
+# npm install
+
+# Additional tasks can be added here as required.
