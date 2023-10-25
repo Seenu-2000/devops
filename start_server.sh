ef07cb9b5179b9ce48a0e8808fdc53f492a4967e
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-25 14:39:40 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
 
-systemctl start apache2
+# Navigate to the home directory of the "ubuntu"
+cd /home/ubuntu
+
+# Start the Node.js application using the "start" script from package.json
+npm start
