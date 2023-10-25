ae6bd72b8fcf40c40b1fc3c7d676e30fb0d369d4
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-25 16:45:48 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Navigate to the home directory of the "ubuntu"
-cd /home/ubuntu
+cd /home/ubuntu/task
 
 # Start the Node.js application using the "start" script from package.json
 npm start
