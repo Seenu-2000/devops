2af9d0560a333199378194c5e004dcc48748f652
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-26 00:26:28 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,5 @@ set -e
 # Navigate to the appropriate directory where nodescript.js is located
 cd /var/www/myapp
 
-# Run your Node.js application
-node nodescript.js
+# Start your Node.js application with PM2
+pm2 start nodescript.js
