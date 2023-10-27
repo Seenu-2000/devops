0a8443a37b63f014ccfaca948d5f89fa64f86a7f
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-28 02:35:59 +0530

    Update nodescript.js

--- nodescript.js
+++ nodescript.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, People this is Node.js ');
+  res.end('Hello, People this is Node.js');
 });
 
 const port = process.env.PORT || 3000;
