6540fbd78d33bf8f8a0f9a9c8990df27950c9f30
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-10-27 23:26:01 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Hook: BeforeInstall
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Node.js and NPM
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Hook: AfterInstall
+sudo mkdir -p /var/www/myapp
+sudo chown -R ubuntu:ubuntu /var/www/myapp
+mv /home/ubuntu/myfiles/package.json /var/www/myapp/
+mv /home/ubuntu/myfiles/nodescript.js /var/www/myapp
+cd /var/www/myapp
+sudo npm install
+sudo npm install -g pm2
+
+# Hook: ApplicationStart
+cd /var/www/myapp
+pm2 start nodescript.js -f
