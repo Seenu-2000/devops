1121dbcb924ad152eaa86ea862c3dd2416cf461f
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2023-11-01 17:41:22 +0530

    Update nodescript.js

--- nodescript.js
+++ nodescript.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, People this is Node.js');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1000;
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
