2055536a6655c30dc16c92e3b9e7638e7ebd7537
Seenu-2000 <148611771+Seenu-2000@users.noreply.github.com>
2024-01-11 20:20:52 +0530

    Delete start_server.sh

--- start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Navigate to the appropriate directory where nodescript.js is located
-cd /var/www/myapp
-
-# Start your Node.js application with PM2, forcing re-execution
-pm2 start nodescript.js -f
